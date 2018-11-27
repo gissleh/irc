d42df0d7a057d6efef315919a178600655d6a2b6
Gisle Aune <dev@gisle.me>
2018-11-27 17:58:44 +0100

    client: Fixed incorrect comand being sent when parting a closed target.

--- client.go
+++ client.go
@@ -600,7 +600,7 @@ func (client *Client) RemoveTarget(target Target) (id string, err error) {
 
 			// Ensure the channel has been parted
 			if channel, ok := target.(*Channel); ok && !channel.parted {
-				client.SendQueuedf("/part %s", channel.Name())
+				client.SendQueuedf("PART %s", channel.Name())
 			}
 
 			return
